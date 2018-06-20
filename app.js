4b22c6ce8b76cc0139aa4095897e8223ca8201d2
Dylan <dylansprague11@hotmail.com>
2018-06-20 15:18:45 -0400

    Botception/ping command setup

--- app.js
+++ app.js
@@ -10,4 +10,20 @@ bot.on("guildCreate", guild => {
     console.log(`I've joined the Discord Server ${guild.name} (${guild.id}), owned by ${guild.owner.user.username} (${guild.owner.user.id}).`);
 });
 
+bot.on("message", function(message){
+    if(message.author.equals(bot.user)) return;
+
+    if(!message.content.startsWith(config.prefix)) return;
+
+    var args = message.content.substring(config.prefix.length).split(" ");
+
+    switch(args[0].toLowerCase()){
+        case "ping":
+            message.channel.send("Pong!");
+            break;
+        default:
+            message.channel.send(":x: Invalid command!");
+    }
+});
+
 bot.login(config.token);
