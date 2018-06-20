b0b5afc5a210fb09a655d297dcde242f568ad647
Dylan <dylansprague11@hotmail.com>
2018-06-20 15:08:42 -0400

    Basic bot setup

---
+++ app.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const config = require('config.json');
+const bot = new Discord.Client();
+
+bot.on("ready", function(){
+    console.log("FyreBot is ready!");
+});
+
+bot.on("guildCreate", guild => {
+    console.log(`I've joined the Discord Server ${guild.name} (${guild.id}), owned by ${guild.owner.user.username} (${guild.owner.user.id}).`);
+});
+
+bot.login(config.token);
